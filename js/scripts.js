b6ec4cb8607f5705927097035a829071197517aa
oidiai <oidiai@icloud.com>
2023-01-16 20:23:23 +0100

    ht.16.jan.carousel

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:60,
+        nav:true,
+        navText: [ '', ' ' ],
+        autoplay:true,
+        autoplayTimeout:2500,
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
