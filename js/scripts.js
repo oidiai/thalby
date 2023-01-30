294c822e78756b749b6ef228753363a73de649f1
oidiai <oidiai@icloud.com>
2023-01-30 19:55:16 +0100

    diploma.30.jan

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    // carousel
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:60,
@@ -18,7 +18,37 @@ $(document).ready(function(){
         }
     });
 
-    $( function() {
-        $( "#accordion" ).accordion();
-    } );
+    //burger menu open
+    $('.menu').on("click", function () {
+        $('header nav ul li').css('display','inherit').css('margin-bottom', '7px');
+        $('.menu').css('display','none');
+        $('.logo').css('top','15px');
+        $('.action_bar').css('align-self','flex-start');
+    });
+    //burger menu close
+    $('.close').on("click", function () {
+        $('header nav ul li').css('display','none');
+        $('.menu').css('display','inherit');
+        $('.logo').css('top','inherit');
+        $('.action_bar').css('align-self','inherit');
+    });
+
+
+    // button scroll to top
+    let btn = $('#to-top');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 400) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(toTop) {
+        toTop.preventDefault();
+        $('html, body').animate({scrollTop:0}, '800');
+    });
 });
+
+
